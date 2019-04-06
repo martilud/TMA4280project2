904be7a3a60019efc2dbf91fa7eb3015f71f971f
haraldwilhelmsen <wil.harald@gmail.com>
2019-04-06 11:41:19 +0200

    add unit tests

--- src/test-files/test_alltoallv.cpp
+++ src/test-files/test_alltoallv.cpp
@@ -1,54 +1,6 @@
 #include <iostream>
 #include <mpi.h>
 
-double fill_matrix(int i, int j, int M, int local_start_n); 
-
-double *mk_1D_array(size_t n, bool zero)
-{
-    if (zero) {
-        return (double *)calloc(n, sizeof(double));
-    }
-    return (double *)malloc(n * sizeof(double));
-}
-
-/*
- * The allocation of the two-dimensional array used for storing matrices is done
- * in the following way for a matrix in R^(n1*n2):
- * 1. an array of pointers is allocated, one pointer for each row,
- * 2. a 'flat' array of size n1*n2 is allocated to ensure that the memory space
- *   is contigusous,
- * 3. pointers are set for each row to the address of first element.
- */
-
-double **mk_2D_array(size_t n1, size_t n2, bool zero)
-{
-    // 1
-    double **ret = (double **)malloc(n1 * sizeof(double *));
-
-    // 2
-    if (zero) {
-        ret[0] = (double *)calloc(n1 * n2, sizeof(double));
-    }
-    else {
-        ret[0] = (double *)malloc(n1 * n2 * sizeof(double));
-    }
-    
-    // 3
-    for (size_t i = 1; i < n1; i++) {
-        ret[i] = ret[i-1] + n2;
-    }
-    return ret;
-}
-
-void printMatrix(double **mat, int N, int M){
-    for (int j = 0; j < M; j++) {
-		for (int i = 0; i < N; i++) {
-			if (mat[i][j] < 10) std::cout << 0;	
-			std::cout << mat[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-}
 
 int main(int argc, char **argv) {
 	// Set up MPI
@@ -75,8 +27,14 @@ int main(int argc, char **argv) {
 	}
 
 	// Create send and recieve buffers
-	double **b = mk_2D_array(local_N,M, 0);
-	double **bt = mk_2D_array(local_N,M, 0);
+	double **b = (double **)malloc(local_N * sizeof(double *));
+	double **bt = (double **)malloc(local_N * sizeof(double *));
+	b[0] = (double *)malloc(local_N * M * sizeof(double));
+	bt[0] = (double *)malloc(local_N * M * sizeof(double));
+    for (size_t i = 1; i < local_N; i++) {
+        b[i] = b[i-1] + M;
+        bt[i] = bt[i-1] + M;
+    }
 
 
 	// Find send and displacement buffers
@@ -96,11 +54,14 @@ int main(int argc, char **argv) {
 	// Fill a with increasing numbers row by row
 	for (int i = 0; i < local_N; i++)
 		for (int j = 0; j < M; j++)
-			b[i][j] = fill_matrix(i, j, M, local_start_N); // i + j*M + local_start_N; 
+			b[i][j] = i + j*M + local_start_N; //fill_matrix(i, j, M, local_start_N); // i + j*M + local_start_N; 
 	
 
+	// Creating a send and recieve temporary 1D array for wrapping/unwrapping the data
+	double *temp = (double *)malloc(local_N * M * sizeof(double));
+	double *recieve_temp = (double *)malloc(local_N * M * sizeof(double));
 
-	double *temp = mk_1D_array(local_N * M, 0);
+
 	int block_M, current_j = 0, count = 0;	
 	for (int block_idx = 0; block_idx < size; block_idx++) {
 		block_M = sendcounts[block_idx] / local_N;
@@ -113,10 +74,6 @@ int main(int argc, char **argv) {
 	}
 
 
-	
-	double *recieve_temp = mk_1D_array(local_N * M, 0);
-
-
 	// Let the magic happen
 	MPI_Alltoallv(&temp[0], sendcounts, sdispls, MPI_DOUBLE, &recieve_temp[0], sendcounts, sdispls, MPI_DOUBLE, MPI_COMM_WORLD);
 
@@ -126,20 +83,22 @@ int main(int argc, char **argv) {
 			bt[i][j] = recieve_temp[count++];
 		}
 	}
-
-	bool correct = true;
 	
+	int correct = 1;
+	int recieve_correct;
+
 	for (int i = 0; i < local_N; i++) {
 		for (int j = 0; j < M; j++) {
-			if (bt[i][j] == fill_matrix(j, i, M, local_start_N)) {
-				correct = false;
+			if (bt[i][j] != j + M * (i + local_start_N)){ //fill_matrix(j, i, M, local_start_N)) {
+				correct = 0;
 			}
 		}
 	}
-	bool recieve_correct;
-	MPI_Reduce(&correct, &recieve_correct, 1, MPI_LOGICAL, MPI_LAND, 0, MPI_COMM_WORLD);
+
+	MPI_Reduce(&correct, &recieve_correct, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
 	if (rank == 0) {
-		if (recieve_correct) {
+		if (recieve_correct == size) {
 			std::cout << "Unit test is succesfull!!" << std::endl;
 		}
 		else {
@@ -148,14 +107,8 @@ int main(int argc, char **argv) {
 	}
 
 
-
 	// Finito!!
 	MPI_Finalize();
 	return 0;
 }
-
-
-double fill_matrix(int i, int j, int M, int local_start_n) {
-	return (i + j*M + local_start_n);
-}	
 
